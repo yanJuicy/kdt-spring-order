27f9bebb1ac37a7b77e70a52fc0be2b4a9702d96
Yanjoo <hanjub3@gmail.com>
2023-03-08 20:14:25 +0900

    코드 정렬

--- src/main/java/org/prgrms/kdt/AppConfiguration.java
+++ src/main/java/org/prgrms/kdt/AppConfiguration.java
@@ -10,33 +10,33 @@ import java.util.UUID;
 public class AppConfiguration {
 
 	@Bean
-    public VoucherRepository voucherRepository() {
-        return new VoucherRepository() {
-            @Override
-            public Optional<Voucher> findById(UUID voucherId) {
-                return Optional.empty();
-            }
-        };
-    }
+	public VoucherRepository voucherRepository() {
+		return new VoucherRepository() {
+			@Override
+			public Optional<Voucher> findById(UUID voucherId) {
+				return Optional.empty();
+			}
+		};
+	}
 
 	@Bean
 	public OrderRepository orderRepository() {
-        return new OrderRepository() {
-            @Override
-            public void insert(Order order) {
+		return new OrderRepository() {
+			@Override
+			public void insert(Order order) {
 
-            }
-        };
-    }
-
-	@Bean
-    public VoucherService voucherService() {
-        return new VoucherService(voucherRepository());
-    }
+			}
+		};
+	}
 
 	@Bean
-    public OrderService orderService() {
-        return new OrderService(voucherService(), orderRepository());
-    }
+	public VoucherService voucherService() {
+		return new VoucherService(voucherRepository());
+	}
+
+	@Bean
+	public OrderService orderService() {
+		return new OrderService(voucherService(), orderRepository());
+	}
 
 }
